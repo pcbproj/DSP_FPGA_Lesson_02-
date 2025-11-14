b5aa571a56187a363eb2a17edb9a9dcdabed41fe
pcbproj <pcbproj@yandex.ru>
2025-11-14 19:33:42 +0300

    geany project added

--- send_repo_to_github.bat
+++ send_repo_to_github.bat
@@ -1,4 +1,4 @@
-git remote add origin https://github.com/pcbproj/LCD1602_interface.git
+git remote add origin https://github.com/pcbproj/DSP_FPGA_Lesson_02-.git
 git branch -M main
 git push -u origin main
 pause
